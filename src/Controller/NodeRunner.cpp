a9e0608c1173b88d879d876c6c11b579a3acf788
Alejandro <AlexMJHS@gmail.com>
2016-01-27 09:31:07 -0700

    Add to repo

---
+++ src/Controller/NodeRunner.cpp
@@ -0,0 +1,15 @@
+/*
+ * NodeRunner.cpp
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: amag6880
+ */
+
+
+int main()
+{
+	return 0;
+}
+
+
+
---
+++ src/Model/Node.h
@@ -0,0 +1,17 @@
+/*
+ * Node.h
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: amag6880
+ */
+
+#ifndef MODEL_NODE_H_
+#define MODEL_NODE_H_
+
+class Node {
+public:
+	Node();
+	virtual ~Node();
+};
+
+#endif /* MODEL_NODE_H_ */
