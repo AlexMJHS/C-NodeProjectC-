de7b4250597ca64e8bb0853ed411b8bdd4645016
Alejandro <AlexMJHS@gmail.com>
2016-02-22 09:50:52 -0700

    Added CTECList

---
+++ src/Model/CTECList.cpp
@@ -0,0 +1,36 @@
+/*
+ * CTECList.cpp
+ *
+ *  Created on: Feb 22, 2016
+ *      Author: amag6880
+ */
+
+#include "CTECList.h"
+
+template <class Type>
+CTECList<Type>::CTECList()
+{
+	this->size = 0;
+	this->head = nullptr;
+	this->end = nullptr;
+
+}
+
+template <class Type>
+CTECList<Type>::~CTECList()
+{
+
+}
+
+template <class Type>
+Type CTECList<Type>:: removeFromFront()
+{
+	//Create a pointer to what is after head.
+	ArrayNode<Type> * newHead = new ArrayNode<Type>();
+	newHead= head->getNext();
+	//Delete what head is pointing to
+	delete this->head;
+	head = newHead;
+	//Set head to the new head
+	this->head = newHead;
+}
